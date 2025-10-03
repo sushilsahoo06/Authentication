955412b6b3e01bc3df3e14cec95711d9a4ed25b9
sushilsahoo06 <sahoosushil456@gmail.com>
2025-10-03 16:11:43 +0530

    commit

---
+++ server/controller/Auth.js
@@ -0,0 +1,148 @@
+const User = require("../../server/models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  const { userName, email, password } = req.body;
+  if (!userName || !email || !password) {
+    return res.status(400).json({ success: false, message: "Some error occured !" });
+  }
+  try {
+    const checkUser = await User.findOne({ email });
+    if (checkUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exist !",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Registration successful",
+      user:{
+        id:savedUser._id,
+        userName:savedUser.userName,
+        email:savedUser.email
+      }
+    });
+  } catch (error) {
+    console.error("Register Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+//login
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and Password Required !",
+    });
+  }
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.json({
+        success: false,
+        message: "Invalid Email",
+      });
+    }
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+
+    if (!checkPassword) {
+      return res.json({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    const token = jwt.sign({ id: checkUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user:{
+        id:checkUser._id,
+        userName:checkUser.userName,
+        email:checkUser.email
+      }
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server Error",
+    });
+  }
+};
+//logout
+
+const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    console.error("Logout Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+const authMiddleware=async(req,res,next)=>{
+  const token =req.cookies.token;
+  if(!token){
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorised user!",
+    });
+  }
+  try{
+    const decoded=jwt.verify(token,process.env.JWT_SECRET);
+    req.user=decoded;
+    next();
+
+  }catch(error){
+    console.error(" UnauthError:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+    
+}
+module.exports={
+  register,login,logout,authMiddleware
+}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema({
-  name:{type:String,required:true},
-  email:{type:String,}
-})
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verifyOTP: { type: String, default: "" },
+  verifyOTPExpireAt: { type: Number, default: 0 },
+  isAccountVerified: { type: Boolean, default: false },
+  resetOtp: { type: Number, default: "" },
+  resetOtpExpireAt: { type: Number, default: "" },
+});
+ const User=mongoose.model("User",userSchema)
+ exports.model=User;
